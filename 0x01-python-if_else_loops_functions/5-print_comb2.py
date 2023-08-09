7ec91cadb3478c571f684f5025ef2cd0ffac01ab
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-08-09 17:38:03 +0100

    print comb2

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,11 +2,6 @@
 
 for num in range(100):
     if num < 10:
-        print("0{:d}".format(num), end="")
+        print("0{:d}".format(num), end=", " if num != 99 else "\n")
     else:
-        print("{:d}".format(num), end="")
-
-    if num != 99:
-        print(", ", end="")
-
-print("")
+        print("{:d}".format(num), end=", " if num != 99 else "\n")
