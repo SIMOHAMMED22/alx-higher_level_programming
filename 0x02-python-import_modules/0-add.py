936f6761dd11666fcea05941ed87d4b68dcb2db1
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-08-10 20:57:34 +0100

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-def add(a, b):
-    return a + b
+from add_0 import add
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     a = 1
     b = 2
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
