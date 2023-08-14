71b5ca853c1d10f10d3bb7962be3a9d2c09c92dc
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-08-14 15:14:56 +0100

    multiple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if not sentence:
-        return (None, None)
-    return (len(sentence), sentence[0])
+    return (len(sentence), sentence[0] if len(sentence) else None)
