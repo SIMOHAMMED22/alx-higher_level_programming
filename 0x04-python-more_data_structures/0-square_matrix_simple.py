74a8113f4a4aea803224b07543dfb44371c29012
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-08-16 16:01:39 +0100

    matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    squared_matrix = []
+    for row in matrix:
+        squared_row = []
+
+        for element in row:
+            squared_value = element ** 2
+            squared_row.append(squared_value)
+
+        squared_matrix.append(squared_row)
+
+    return squared_matrix
