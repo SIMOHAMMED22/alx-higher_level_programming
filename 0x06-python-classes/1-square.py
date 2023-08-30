68aeca39d522a1f2d8116163561513c1ca8ffea5
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-08-30 14:16:25 +0100

    square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Class Square Package."""
+
 
 class Square:
+    """Square Class."""
+
     def __init__(self, size):
+        """Square constructor."""
         self.__size = size
