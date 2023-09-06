0a3d8eb9d311e2243521db1f88a8bd55070d4c9a
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-09-06 23:03:55 +0100

    locked

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,9 +4,12 @@
 
 
 class LockedClass:
-    """Body of LockedClass class"""
+    """Body of LockedClass class
+    """
     def __setattr__(self, name, val):
-        """Limits which attributes can be set"""
+        """Limits which attributes
+        can be set
+        """
         if name == "first_name":
             self.__dict__[name] = val
         else:
