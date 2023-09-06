539eaf5aa031bbe90c2b0abc219c2aa90dc289d0
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-09-06 22:54:36 +0100

    locked

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Locked class module
+"""
+
+
+class LockedClass:
+    """Body of LockedClass class
+    """
+    def __setattr__(self, name, val):
+        """Limits which attributes can be set
+        """
+        if name == "first_name":
+            self.__dict__[name] = val
+        else:
+            err_msg = f"'LockedClass' object has no attribute '{name}'"
+            raise AttributeError(err_msg)
