4ca41c3c3a05ea0edb11c3dc61e2c2d886ec3291
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-09-11 16:34:12 +0100

    SAME CLASS

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defining an object attribute lookup function."""
+
+
+def is_same_class(obj, a_class):
+    """
+    Check if an object is exactly an instance of a specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        True if obj is an instance of a_class, False otherwise.
+    """
+    return isinstance(obj, a_class)
