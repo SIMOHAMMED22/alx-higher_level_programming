3719dc28184d06a00a43fd9963e32e7f4a956827
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-09-12 15:18:00 +0100

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,31 +1,8 @@
 #!/usr/bin/python3
-"""read file"""
+"""Defines a function for text file-reading."""
 
 
 def read_file(filename=""):
-    """
-    Read a text file and print its contents to stdout.
-
-    Args:
-        filename (str, optional): The name
-        of the text file. Defaults to an empty string.
-
-    Returns:
-        None
-
-    Note:
-        - Uses 'with' statement to open and close the file.
-        - Reads the file line by line and prints each line to stdout.
-        - Handles FileNotFoundError and other exceptions.
-    """
-    try:
-        with open(filename, 'r', encoding='utf-8') as file:
-            for line in file:
-                print(line, end='')
-    except FileNotFoundError:
-        print(f"File '{filename}' not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-
-read_file("example.txt")
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
