00a1999e8e390dd4cdfee59f410369bca25d1ff4
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-09-13 15:25:55 +0100

    class

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+""" function that returns the dictionary. """
 
+
+def class_to_json(obj):
+    """ Function retuns dictionary """
+    dic = {}
+    if hasattr(obj, "__dict__"):
+        dic = obj.__dict__.copy()
+    return dic
 def class_to_json(obj):
     """
     Returns a dictionary representation of an object for JSON serialization.
