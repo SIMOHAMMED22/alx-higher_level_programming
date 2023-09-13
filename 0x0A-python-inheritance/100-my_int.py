542232f2747090435996ef22239d9c2405b3e088
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-09-13 17:38:59 +0100

    201

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Create a class that inherit from int """
+
+
+class MyInt(int):
+    def __eq__(self, other):
+        """
+        Override the equality (==) operator to invert its behavior.
+
+        Args:
+            other: The value to compare with.
+
+        Returns:
+            bool: True if the values are not equal, False otherwise.
+        """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """
+        Override the inequality (!=) operator to invert its behavior.
+
+        Args:
+            other: The value to compare with.
+
+        Returns:
+            bool: True if the values are equal, False otherwise.
+        """
+        return super().__eq__(other)
