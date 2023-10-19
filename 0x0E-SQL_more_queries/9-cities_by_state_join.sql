905eca3ae4b5ffc947fd6211d8d961d77f2c8f1d
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-10-19 16:58:55 +0100

    JOIN

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -5,5 +5,5 @@
 -- Select all cities with their corresponding state names
 SELECT cities.id, cities.name, states.name AS state_name
 FROM cities
-INNER JOIN states ON cities.state_id = states.id
+LEFT JOIN states ON cities.state_id = states.id
 ORDER BY cities.id;
