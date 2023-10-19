550668a6624de057738ec5904cce44efa447973a
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-10-19 16:56:42 +0100

    JOIN

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,9 @@
+-- Connect to the MySQL server and specify the database
+-- You can use the following command to connect to the MySQL server with the specified database:
+-- mysql -u your_admin_username -p your_database_name
+
+-- Select all cities with their corresponding state names
+SELECT cities.id, cities.name, states.name AS state_name
+FROM cities
+INNER JOIN states ON cities.state_id = states.id
+ORDER BY cities.id;
