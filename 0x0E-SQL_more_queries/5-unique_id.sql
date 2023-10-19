2d03148d7ea40b6754afd3a36525d299c4cfb212
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-10-19 16:29:15 +0100

    UNIQUE

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -3,9 +3,6 @@
 -- mysql -u your_admin_username -p your_database_name
 
 -- Create or update the 'id_not_null' table if it doesn't already exist
-CREATE TABLE IF NOT EXISTS id_not_null (
-    id INT DEFAULT 1,
-    name VARCHAR(256)
-);
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
 
 -- Note: Make sure to replace 'your_admin_username' and 'your_database_name' with the actual MySQL administrative username and the desired database name.
