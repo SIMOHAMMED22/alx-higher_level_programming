ae3bd0fff57fbb24a89f9b1705c79959175be5dd
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-10-19 17:07:04 +0100

    genre

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,7 +2,7 @@
 -- You can use the following command to connect to the MySQL server with the specified database:
 -- mysql -u your_admin_username -p your_database_name
 
-
+-- List shows with at least one linked genre
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
