02f965ac23f55d847fe0baa7a4c13f80985b04ab
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-10-19 16:00:14 +0100

    privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,9 @@
--- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
-FROM information_schema.user_privileges
-WHERE grantee = 'user_0d_1@localhost' OR grantee = 'user_0d_2@localhost';
+-- Connect to the MySQL server
+-- You can use the following command to connect to the MySQL server:
+-- mysql -u your_admin_username -p
+
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
