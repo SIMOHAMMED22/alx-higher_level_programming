dbd237cc097e0c7130b1d59c2002844c078146de
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-10-19 17:02:14 +0100

    GENRE

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- Connect to the MySQL server and specify the database
+-- You can use the following command to connect to the MySQL server with the specified database:
+-- mysql -u your_admin_username -p your_database_name
+
+-- List shows with at least one linked genre
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
