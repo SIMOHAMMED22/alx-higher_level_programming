4dd6b9c81047ce551848c42cda8717de5dcb2b1c
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-11-14 16:22:23 +0100

    multi

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-const language1 = "C is fun";
-const language2 = "Python is cool";
-const language3 = "JavaScript is amazing";
+const myVar = "C is fun\nPython is cool\nJavaScript is amazing";
 
-console.log(language1);
-console.log(language2);
-console.log(language3);
+console.log(myVar);
