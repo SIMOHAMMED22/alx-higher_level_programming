e5b8307727ba03c32bcb689ca4284cfdbae7bc77
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-11-14 18:44:35 +0100

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-function computeFactorial (n) {
-  if (isNaN(n)) {
-	  return 1;
-  } else if (n <= 1) {
-	  return 1;
-  } else {
-	  return n * computeFactorial(n - 1);
+const args = process.argv;
+const n = parseInt(args[2]);
+function factorial (n) {
+  if (n === 0 || n === 1 || n === 'NaN') {
+    return (1);
   }
+  return n * factorial(n - 1);
 }
-
-const number = parseInt(process.argv[2]);
-
-console.log(computeFactorial(number));
+if (!isNaN(n)) {
+  console.log(factorial(n));
+} else {
+  console.log(1);
+}
