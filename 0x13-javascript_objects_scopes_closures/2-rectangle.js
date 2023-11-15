af9b18d31e66f3756aba2bc95076ed968bfbfa88
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-11-15 23:11:58 +0100

    2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      return {};
-    } else {
-      this.width = w;
-      this.height = h;
-    }
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) { return 'Rectangle{}'; }
+    this.width = w;
+    this.height = h;
   }
-}
-module.exports = Rectangle;
+};
