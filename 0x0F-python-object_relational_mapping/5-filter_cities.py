69f977f13cc55c89ae6ebd7cee514af5dd42ae88
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-11-20 13:26:43 +0100

    flter

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python3
 """getting started with the python module MySQLdb"""
-
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
 
     db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=db_name
-    )
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+        )
 
     cur = db.cursor()
-    query = "SELECT cities.name FROM cities JOIN states \
-    ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id"
-    cur.execute(query, (state_name,))
-    rows = cur.fetchall()
+    sql = "select c.name from states as s " \
+        " inner join cities as c on c.state_id = s.id and s.name = %s"
 
-    cities = list(set(row[0] for row in rows))
+    cur.execute(sql, (sys.argv[4], ))
 
-    city_order = ["Dallas", "Houston", "Austin"]
+    res = cur.fetchall()
 
-    filtered_cities = [city for city in city_order if city in cities]
-
-    print(', '.join(filtered_cities))
-
+    for i in range(len(res)):
+        if i == len(res) - 1:
+            print(res[i][0], end="")
+        else:
+            print(f"{res[i][0]}, ", end="")
+    print()
     cur.close()
     db.close()
