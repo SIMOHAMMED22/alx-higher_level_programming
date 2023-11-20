ac53c2c185ad96e00b798e6249a9827c56114fd3
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-11-20 15:36:25 +0100

    state

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,34 +1,22 @@
 #!/usr/bin/python3
-"""Script to print specific State objects from the database hbtn_0e_6_usa"""
+"""Fetch All the Data From the table states"""
 import sys
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, select
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
-
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, database))
-
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    sentence = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine(sentence.format(
+        username, password, database), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    query = session.query(State)
-    query = query.filter(State.id.in_([1, 2, 3, 5]))
-    query = query.order_by(State.id)
-    states = query.all()
-
-    if states:
-        for state in states:
-            print("{}: {}".format(state.id, state.name))
-    else:
-        print("Nothing")
-
-    session.close()
+    states = session.query(State).\
+        filter(State.name.like(f'%a%')).order_by(State.id.asc()).all()
+    for state in states:
+        print('{}: {}'.format(state.id, state.name))
