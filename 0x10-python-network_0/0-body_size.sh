d77702b090df7668498f78eb66afaa27f47f3dbd
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-11-30 15:42:56 +0100

    size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-url=$1
-
-response=$(curl -s -o /dev/null -w "%{size_download}" "http://$url")
-
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to fetch the URL."
-  exit 1
-fi
-
-echo "Size of the body of the response from $url is: $response bytes"
+# Getting the byte size of the HTTP response header for a given URL.
+curl -s "$1" | wc -c
