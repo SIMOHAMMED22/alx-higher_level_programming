89b5cb4d544588b778359a26ec004b918cd45089
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-11-30 23:58:50 +0100

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-""" implement find_peak """
+"""Define a find peak algo"""
 
 
 def find_peak(list_of_integers):
-    """find_peak
-
-    Args:
-        list_of_integers (list): list of integers
-
-    Returns:
-        integer: the peak of the given list
-    """
-    if len(list_of_integers) == 0:
+    """Return the peak"""
+    if not list_of_integers:
         return None
-    peak = list_of_integers[0]
-    for ele in list_of_integers:
-        if ele > peak:
-            peak = ele
-    return peak
+
+    left = 0
+    right = len(list_of_integers) - 1
+
+    while left < right:
+        mid = (left + right) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+
+    return list_of_integers[left]
