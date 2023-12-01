c92db7e912823d53852485a9f51896dc93f913f6
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-12-01 23:50:32 +0100

    email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,18 +4,15 @@ Sends a POST request to the specified
 URL with the email as a parameter,
 and displays the body of the response.
 """
-
-
-import urllib.parse
 import urllib.request
+import urllib.parse
 import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
 
-data = urllib.parse.urlencode({'email': email})
-data = data.encode('utf-8')
-req = urllib.request.Request(url, data=data, method='POST')
-
-with urllib.request.urlopen(req) as response:
-    print(response.read().decode('utf-8'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = urllib.parse.urlencode({'email': email}).encode()
+    req = urllib.request.Request(url, data=data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
