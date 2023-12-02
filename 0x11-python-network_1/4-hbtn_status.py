d8744f24f5c5b9765a1c047a8c6b6ba7c88b06e4
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-12-02 15:17:30 +0100

    hbtn

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,25 +1,13 @@
 #!/usr/bin/python3
 """Python script that fetches
 https://alx-intranet.hbtn.io/status"""
-
-
-import urllib.request
-import urllib.error
-import sys
-
-
-def fetch_url_body(url):
-    try:
-        with urllib.request.urlopen(url) as response:
-            body = response.read().decode('utf-8')
-            print(body)
-    except urllib.error.HTTPError as e:
-        print(f"Error code: {e.code}")
-
+import requests
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        url = sys.argv[1]
-        fetch_url_body(url)
-    else:
-        print("Error: Please provide a URL as an argument.")
+    url = "https://alx-intranet.hbtn.io/status"
+
+    response = requests.get(url)
+
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
