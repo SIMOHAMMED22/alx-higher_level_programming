2ca312aa440942b5dc158cfff3d3fcd227199f73
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-12-04 15:21:40 +0100

    error

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,23 +6,12 @@ displays the body of the response."""
 import requests
 import sys
 
-
-def fetch_url_content(url):
-    try:
-        response = requests.get(url)
-        print("Response body:")
-        print(response.text)
-
-        if response.status_code >= 400:
-            print(f"Error code: {response.status_code}")
-    except requests.RequestException as e:
-        print(f"Failed to fetch URL: {e}")
-
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script_name.py <URL>")
-        sys.exit(1)
-
     url = sys.argv[1]
-    fetch_url_content(url)
+
+    response = requests.get(url)
+
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
