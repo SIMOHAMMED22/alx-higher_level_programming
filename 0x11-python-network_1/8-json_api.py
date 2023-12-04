cf9461b79aa0c1d39d3b9b03c34e3bdb8ef50e9a
MOHAMMED EL FAQYH <simofinance22@gmail.com>
2023-12-04 15:51:33 +0100

    json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,33 +1,18 @@
 #!/usr/bin/python3
-"""sends a POST request ,query"""
-
-
-import requests
-import sys
-
-
-def search_user(letter):
-    url = 'http://0.0.0.0:5000/search_user'
-    params = {'q': letter}
-
-    try:
-        response = requests.post(url, params=params)
-        json_data = response.json()
-
-        if json_data:
-            if 'id' in json_data and 'name' in json_data:
-                print(f"[{json_data['id']}] {json_data['name']}")
-            else:
-                print("Not a valid JSON")
-        else:
-            print("No result")
-
-    except ValueError:
-        print("Not a valid JSON")
-    except requests.RequestException as e:
-        print(f"Request Error: {e}")
-
+"""8. Search API"""
 
 if __name__ == "__main__":
-    letter = sys.argv[1] if len(sys.argv) > 1 else ""
-    search_user(letter)
+    import requests
+    import sys
+
+    url = 'http://0.0.0.0:5000/search_user'
+    arg = sys.argv[1] if len(sys.argv) == 2 else ""
+    r = requests.post(url, data={'q': arg})
+    try:
+        g = r.json()
+        if g == {}:
+            print('No result')
+        else:
+            print(g)
+    except Exception:
+        print("Not a valid JSON")
