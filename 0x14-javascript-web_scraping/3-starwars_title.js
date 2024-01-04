b48a5a541d5babf6367b1c6dc901d4babfc640c5
MOHAMMED EL FAQYH <125474509+SIMOHAMMED22@users.noreply.github.com>
2024-01-04 19:19:17 +0100

    Create 3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  console.log(JSON.parse(body).title);
+});
