d0f9796be90ed25d0ce55e8f69eb40bb1bb044ac
MOHAMMED EL FAQYH <125474509+SIMOHAMMED22@users.noreply.github.com>
2024-01-12 17:47:15 +0100

    Create 4-script.js

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,12 @@
+$(function () {
+  const header = $('header');
+  $('div#toggle_header').on('click', () => {
+    if (header.hasClass('red')) {
+      header.removeClass('red');
+      header.addClass('green');
+    } else {
+      header.removeClass('green');
+      header.addClass('red');
+    }
+  });
+});
